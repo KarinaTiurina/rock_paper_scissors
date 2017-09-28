09d697b44386f0505a7d6a1816e77ad2d02f1d0d
KarinaTiurina <rktiurina@gmail.com>
2017-09-28 10:38:55 +0300

    code style corrections

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -7,43 +7,28 @@ class GamesController < ApplicationController
   def show
     @player_choice = params[:id].to_i
     @computer_choice = rand(3)
-    @winner = winner
+    @winner = determine_the_winner(@player_choice, @computer_choice)
   end
 
   def set_game
     @game = ['rock', 'scissors', 'paper']
   end
 
-  def winner
-    result = determine_the_winner(@player_choice, @computer_choice)
-    if result == 1
-      :player
-    elsif result == 2
-      :computer
+  def determine_the_winner(first_player, second_player)
+    first_player = first_player.to_i
+
+    second_player = second_player.to_i
+
+    if first_player == second_player
+      return :draw
+    end
+
+    if first_player == 0 && second_player == 1 ||
+      first_player == 1 && second_player == 2 ||
+      first_player == 2 && second_player == 0
+      return :player
     else
-      :draw
+      return :computer
     end
   end
-
-  def determine_the_winner(first_player, second_player)
-  if first_player == nil || !first_player.is_a?(Numeric)
-    first_player = 0
-  end
-
-  if second_player == nil || !second_player.is_a?(Numeric)
-    second_player = 0
-  end
-
-  if first_player == second_player
-    return 0
-  end
-
-  if first_player == 0 && second_player == 1 ||
-    first_player == 1 && second_player == 2 ||
-    first_player == 2 && second_player == 0
-    return 1
-  else
-    return 2
-  end
-end
 end
